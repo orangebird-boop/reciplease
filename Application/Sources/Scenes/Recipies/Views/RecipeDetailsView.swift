feef99394e47a1268092cb5556f3c87c38798db7
Nora Lilla Matyassi <orangebirdboop@gmail.com>
2022-05-18 13:47:04 +0200

    ..

--- Application/Sources/Scenes/Recipies/Views/RecipeDetailsView.swift
+++ Application/Sources/Scenes/Recipies/Views/RecipeDetailsView.swift
@@ -1,8 +1,40 @@
-//
-//  RecipeDetailsView.swift
-//  Application
-//
-//  Created by Nora Lilla Matyassi on 18/05/2022.
-//
 
-import Foundation
+import UIKit
+
+protocol RecipeDetailsViewDelegate: AnyObject {
+    
+}
+class RecipeDetailsView: UIView {
+    let recipeTitle = UILabel()
+    let recipeDetails = UITextView()
+    
+    weak var delegate: RecipeDetailsViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupRecipeDetailsView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupRecipeDetailsView() {
+        setupView()
+        setupLayout()
+    }
+    
+    private func setupView() {
+        recipeTitle.text = "From Euro:"
+        recipeTitle.font = UIFont.preferredFont(forTextStyle: .title1)
+        recipeTitle.textAlignment = .left
+        recipeTitle.textColor = .darkGray
+        addSubview(recipeTitle)
+        
+        euroTextView.font = UIFont.preferredFont(forTextStyle: .footnote)
+        euroTextView.textAlignment = .left
+        euroTextView.keyboardType = .numberPad
+        euroTextView.layer.cornerRadius = 10
+        addSubview(euroTextView)
+    }
+}
