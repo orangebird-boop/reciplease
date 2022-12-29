648565a1316161d5f2f50ad17acbc5fc901db705
Nora Lilla Matyassi <orangebirdboop@gmail.com>
2022-12-29 15:49:40 +0100

    alert noMatch

--- Application/Sources/App/Scenes/Search/SearchViewModel.swift
+++ Application/Sources/App/Scenes/Search/SearchViewModel.swift
@@ -4,12 +4,14 @@ enum SearchViewModelError: LocalizedError {
     case failedToRetrieveRecipes
     case defaultError
     case failedToUpdateIngredients
+//    case ingredientError
     
     var helpAnchor: String? {
         switch self {
         case .failedToRetrieveRecipes: return "Failed to retrive recipes"
         case .defaultError: return "An error occured, please contact our support team on the following adresse: support@reciplease.com"
         case .failedToUpdateIngredients: return "This ingredient is already in your list"
+//        case .ingredientError: return "Failed to retrive recipes, an ingredient is not valid"
         }
     }
 }
@@ -18,6 +20,7 @@ protocol SearchViewModelDelegate: AnyObject {
     func didNotUpdateIngredients(error: SearchViewModelError)
     func didFindRecipes()
     func didNotFindRecipe(error: SearchViewModelError)
+    func noMatch()
 }
 
 class SearchViewModel {
@@ -77,6 +80,9 @@ class SearchViewModel {
 
                 case .invalidData, .invalidResponse, .invalidJSONStructure:
                     self.delegate?.didNotFindRecipe(error: .defaultError)
+                
+                default:
+                    self.delegate?.noMatch()
                 
                 }
             }
